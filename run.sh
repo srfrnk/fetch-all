384b88361a702eb2b15acb13981eb3124a97eb3c
srfrnk <srfrnk@gmail.com>
2016-01-26 08:47:16 +0200

    another fix for bash runner

--- run.sh
+++ run.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-if [$(which nodejs) = ""]
-then
+
+if [ -n "$(which node)" ]; then
     node="node"
 else
     node="nodejs"
 fi
 
-$node $(dirname $(readlink -f $0))/fetch-all.js
-
+"$node" $(dirname $(readlink -f $0))/fetch-all.js
